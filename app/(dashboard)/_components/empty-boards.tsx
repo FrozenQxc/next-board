7765745e00b2f50c7f308a61624b3f78139e6cdd
Timofei Stepanov <125030768+FrozenQxc@users.noreply.github.com>
2024-03-08 14:45:05 +0300

    :alien: create mutation

--- app/(dashboard)/_components/empty-boards.tsx
+++ app/(dashboard)/_components/empty-boards.tsx
@@ -1,7 +1,28 @@
+'use client'
+
 import { Button } from '@/components/ui/button'
+import { api } from '@/convex/_generated/api'
+import { useApiMutation } from '@/hooks/use-api-mutation'
+import { useOrganization } from '@clerk/nextjs'
 import Image from 'next/image'
+import { toast } from 'sonner'
 
 export const EmptyBoards = () => {
+	const { organization } = useOrganization()
+	const { mutate, pending } = useApiMutation(api.board.create)
+
+	const onClick = () => {
+		if (!organization) return
+
+		mutate({
+			orgId: organization.id,
+			title: 'Untitled',
+		})
+			.then(id => {
+				toast.success('Доска создана!')
+			})
+			.catch(() => toast.error('Произошла ошибка при создание доски'))
+	}
 	return (
 		<div className='h-full flex flex-col items-center justify-center'>
 			<Image src='/note.svg' height={140} width={140} alt='empty' />
@@ -12,7 +33,9 @@ export const EmptyBoards = () => {
 				Начните с создания доски для вашей организации
 			</p>
 			<div className='mt-6'>
-				<Button size='lg'>Создать доску</Button>
+				<Button disabled={pending} onClick={onClick} size='lg'>
+					Создать доску
+				</Button>
 			</div>
 		</div>
 	)
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import { ConvexClientProvider } from '@/providers/convex-client-provider'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import { Toaster } from 'sonner'
 import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -21,6 +22,7 @@ export default function RootLayout({
 	return (
 		<html lang='en'>
 			<body className={inter.className}>
+				<Toaster />
 				<ConvexClientProvider>{children}</ConvexClientProvider>
 			</body>
 		</html>
